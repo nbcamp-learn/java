e7add23dc36de0fb1e94c70fbe20fcee0b95edfd
eevee <le2yj94@gmail.com>
2024-08-21 23:59:34 +0900

    :sparkles: Add comment functionality to class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
+        // System.out.print & println 사용법
         System.out.println("Hello, Java!");
+
+        System.out.print(7);
+        System.out.println(3);
+        System.out.println(3.14);
+        System.out.println("JAVA");
+
+        // 주석
+        /*
+        * 실제 츠로그램 실핼에는 영향을 미치지 않는 유령코드.
+        * 개발자들끼리 협업할때 코드에 대한 설명을 추가하거나 주의사항 메모할 때 사용.
+        */
+        String data = "데이터";     // 출력할 데이터 문자열
+        System.out.println(data); // 데이터를 출력한다
     }
 }
