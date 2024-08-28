3f26b61c89dd91294b96d27f7ea95f4869ee5652
Eevee <le2yj94@gmail.com>
2024-08-29 00:10:31 +0900

    .

--- src/W01/Main.java
+++ src/W01/Main.java
@@ -3,5 +3,12 @@ package W01;
 public class Main {
     public static void main(String[] args) {
 
+        // System.out.print & println 사용법
+        System.out.println("Hello, Java!");
+
+        System.out.print(7);
+        System.out.println(3);
+        System.out.println(3.14);
+        System.out.println("JAVA");
     }
 }
