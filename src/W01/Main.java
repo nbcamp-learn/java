1ad11a2786722579a7b9e163df169c32eeb310d9
Eevee <le2yj94@gmail.com>
2024-10-10 00:07:45 +0900

    :arrows_counterclockwise: Convert strings to numbers

--- src/W01/Main.java
+++ src/W01/Main.java
@@ -78,9 +78,15 @@ public class Main {
         // 숫자 -> 문자
         Scanner sc = new Scanner(System.in);
 
-        int asciiNumber = sc.nextInt();
-        char ch = (char) asciiNumber;   // 문자로 형변환을 해주면 숫자에 맞는 문자로 표현됨
+//        int asciiNumber = sc.nextInt();
+//        char ch = (char) asciiNumber;   // 문자로 형변환을 해주면 숫자에 맞는 문자로 표현됨
 
-        System.out.println(ch);
+//        System.out.println(ch);
+
+        // 문자 -> 숫자
+        char letter = sc.nextLine().charAt(0);  // 첫번째 글자만 받아오기 위해 charAt(0) 메서드를 사용한다.
+        int asciiNumber = (int) letter;
+
+        System.out.println(asciiNumber);
     }
 }
