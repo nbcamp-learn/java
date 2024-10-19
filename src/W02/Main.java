5b00d2e05a2243c47f9c023f01015883284837dd
Eevee <le2yj94@gmail.com>
2024-10-19 22:47:27 +0900

    :sparkles: Create W02 Main class

---
+++ src/W02/Main.java
@@ -0,0 +1,14 @@
+package W02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* 연산자 */
+        int x = 5;
+        int y = 10;
+
+        int total = x + y;
+        System.out.println(total);
+    }
+}
