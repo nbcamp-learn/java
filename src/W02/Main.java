b50f26c5e90a211dcbe2c211dc4610631fbc8c0c
Eevee <le2yj94@gmail.com>
2024-10-20 00:08:14 +0900

    :heavy_plus_sign: Add basic arithmetic operators

--- src/W02/Main.java
+++ src/W02/Main.java
@@ -10,5 +10,15 @@ public class Main {
 
         int total = x + y;
         System.out.println(total);
+
+        // 사칙 연산
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+        System.out.println(4 / 2); // 2
+        System.out.println(5 / 2); // 2
+        System.out.println(2 / 4); // 0
+        System.out.println(4 % 2); // 0
+        System.out.println(5 % 2); // 1
     }
 }
