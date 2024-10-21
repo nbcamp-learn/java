dbd5ac512c489616caaff87f058b1c1bc0768126
Eevee <le2yj94@gmail.com>
2024-10-21 23:10:51 +0900

    :1234: Add operator precedence handling

--- src/W02/Main.java
+++ src/W02/Main.java
@@ -20,5 +20,10 @@ public class Main {
         System.out.println(2 / 4); // 0
         System.out.println(4 % 2); // 0
         System.out.println(5 % 2); // 1
+
+        // 우선 순위 연산
+        System.out.println(2 + 2 * 2); // 6
+        System.out.println((2 + 2) * 2); // 8
+        System.out.println(2 + (2 * 2)); // 6
     }
 }
