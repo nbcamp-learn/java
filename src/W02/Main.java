79021c07e4a0ff399a905e65afe1b3b78828c151
Eevee <le2yj94@gmail.com>
2024-10-24 23:18:16 +0900

    :heavy_plus_sign: Add comparison operators

--- src/W02/Main.java
+++ src/W02/Main.java
@@ -50,5 +50,15 @@ public class Main {
         // 나눗셈 (나머지) = 나머지 연산
         c = a % b;
         System.out.println(c); // 0
+
+        // 비교 연산자 (참이면 true, 거짓이면 false)
+        System.out.println(10 > 9); // 10 는 9 보다 크다 (참이면 true, 거짓이면 false)
+        System.out.println(10 >= 9); // 10 는 9 보다 크거나 같다 (true)
+        System.out.println(10 < 9); // 10 는 9 보다 작다 (false)
+        System.out.println(10 <= 9); // 10 는 9 보다 작거나 같다 (false)
+        System.out.println(10 == 10); // 10 는 10 와 같다 (true)
+        System.out.println(10 == 9); // 10 는 9 과 같다 (false)
+        System.out.println(10 != 10); // 10 는 10 와 같지 않다 (false)
+        System.out.println(10 != 9); // 10 는 9 과 같지 않다 (true)
     }
 }
