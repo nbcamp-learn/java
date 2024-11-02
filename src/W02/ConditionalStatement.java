add1b52ab7fdc8715b1e943e5bbd46cace67b4f8
Eevee <le2yj94@gmail.com>
2024-11-02 23:10:30 +0900

    :heavy_plus_sign: Add conditional statement implementation

--- src/W02/ConditionalStatement.java
+++ src/W02/ConditionalStatement.java
@@ -4,6 +4,13 @@ public class ConditionalStatement {
 
     public static void main(String[] args) {
 
+        // 조건문
+        boolean flag = true;
+
+        if (flag) {
+            System.out.println("flag 값은 true 입니다."); // flag 값은 true 입니다. 출력
+        }
+
 
     }
 }
