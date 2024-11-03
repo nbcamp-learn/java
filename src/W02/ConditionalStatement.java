6fc9a8286b117ff92e8591d5b19efc85126bebd1
Eevee <le2yj94@gmail.com>
2024-11-03 22:45:25 +0900

    :heavy_plus_sign: Add conditional statement with else

--- src/W02/ConditionalStatement.java
+++ src/W02/ConditionalStatement.java
@@ -11,6 +11,12 @@ public class ConditionalStatement {
             System.out.println("flag 값은 true 입니다."); // flag 값은 true 입니다. 출력
         }
 
-
+        // 조건문 with else
+        boolean flag1 = false;
+        if (flag1) {
+            System.out.println("flag1 값은 true 입니다.");   // 미출력
+        } else {
+            System.out.println("flag1 값은 false 입니다. "); // flag 값은 false 입니다. 출력
+        }
     }
 }
