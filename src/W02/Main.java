636c32c4f8acd136a8eb46700686fc91de2c2863
Eevee <le2yj94@gmail.com>
2024-11-05 20:50:35 +0900

    :heavy_plus_sign: 산술 연산자 - 사칙연산

--- src/W02/Main.java
+++ src/W02/Main.java
@@ -5,6 +5,54 @@ public class Main {
     public static void main(String[] args) {
 
         /* 연산자 */
+        int x = 11;
+        int y = 4;
+        System.out.println(x + y);  // 따로 변수를 생성하지 않고 출력할 수 있음
+
+        int z = x + y;
+        System.out.println(z);
+
+        /* 산술 연산자 */
+        // 사칙 연산자
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+        System.out.println(4 / 2); // 2
+        System.out.println(5 / 2); // 2
+        System.out.println(2 / 4); // 0
+        System.out.println(4 % 2); // 0
+        System.out.println(5 % 2); // 1
+
+        // 우선 순위 연산
+        System.out.println(2 + 2 * 2); // 6
+        System.out.println((2 + 2) * 2); // 8
+        System.out.println(2 + (2 * 2)); // 6
+
+        // 변수를 이용한 연산
+        int a = 20;
+        int b = 10;
+        int c;
+
+        // 덧셈
+        c = a + b;
+        System.out.println(c); // 30
+
+        // 뺄셈
+        c = a - b;
+        System.out.println(c); // 10
+
+        // 곱셈
+        c = a * b;
+        System.out.println(c); // 200
+
+        // 나눗셈 (몫)
+        c = a / b;
+        System.out.println(c); // 2
+
+        // 나눗셈 (나머지) = 나머지 연산
+        c = a % b;
+        System.out.println(c); // 0
+
 
     }
 }
