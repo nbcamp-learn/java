7533c5cb2986fd544bf0cb0d8e445e9f6f65e0ad
Eevee <le2yj94@gmail.com>
2024-11-07 21:01:04 +0900

    :heavy_plus_sign: 논리 연산자

--- src/W02/Main.java
+++ src/W02/Main.java
@@ -65,5 +65,38 @@ public class Main {
         System.out.println(10 != 10); // 10 는 10 와 같지 않다 (false)
         System.out.println(10 != 9); // 10 는 9 과 같지 않다 (true)
 
+        // 논리 연산자
+        // 비교 연산의 결과값으로 받을 수 있는 boolean 값을 연결하는 연산자
+        // && (AND = 피연산자 모두 참), || (OR = 피연산자 둘중 하나라도 참), ! (피연산자의 반대 boolean 값)
+        boolean flag1 = true;
+        boolean flag2 = true;
+        boolean flag3 = false;
+        System.out.println(flag1); // true
+        System.out.println(flag2); // true
+        System.out.println(flag3); // false
+
+        // 피연산자 중 하나라도 true 이면 true
+        System.out.println(flag1 || flag2); // true
+        System.out.println(flag1 || flag2 || flag3); // true
+
+        // 피연산자 모두 true 이면 true
+        System.out.println(flag1 && flag2); // true (flag1, flag2 모두 true 라서)
+        System.out.println(flag1 && flag2 && flag3); // false (flag3은 false 라서)
+
+        // AND 연산
+        System.out.println((5 > 3) && (3 > 1)); // 5 는 3 보다 크고, 3 은 1 보다 크다 (true)
+        System.out.println((5 > 3) && (3 < 1)); // 5 는 3 보다 크고, 3 은 1 보다 작다 (false)
+
+        // OR 연산
+        System.out.println((5 > 3) || (3 > 1)); // 5 는 3 보다 크거나, 3 은 1 보다 크다 (true)
+        System.out.println((5 > 3) || (3 < 1)); // 5 는 3 보다 크거나, 3 은 1 보다 작다 (true)
+        System.out.println((5 < 3) || (3 < 1)); // 5 는 3 보다 작거나, 3 은 1 보다 작다 (false)
+        // System.out.println(1 < 3 < 5); // 불가능한 코드
+
+        // 논리 부정 연산자
+        System.out.println(!flag1); // false (flag1 값의 반대)
+        System.out.println(!flag3); // true (flag3 값의 반대)
+        System.out.println(!(5 == 5)); // false
+        System.out.println(!(5 == 3)); // true
     }
 }
