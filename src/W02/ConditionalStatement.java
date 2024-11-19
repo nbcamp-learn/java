b7fdcc0252556ebddedc1cf39c437af0ed4156b2
Eevee <le2yj94@gmail.com>
2024-11-19 23:09:57 +0900

    :memo: 내용 추가

--- src/W02/ConditionalStatement.java
+++ src/W02/ConditionalStatement.java
@@ -8,15 +8,15 @@ public class ConditionalStatement {
         boolean flag = true;
 
         if (flag) {
-            System.out.println("flag 값은 true 입니다."); // flag 값은 true 입니다. 출력
+            System.out.println("flag 값은 true 입니다."); // flag 값은 true 입니다. 출력됨
         }
 
         // 조건문 (If - Else문)
         boolean flag1 = false;
         if (flag1) {
-            System.out.println("flag1 값은 true 입니다.");   // 미출력
+            System.out.println("flag1 값은 true 입니다.");   // 출력안됨
         } else {
-            System.out.println("flag1 값은 false 입니다. "); // flag 값은 false 입니다. 출력
+            System.out.println("flag1 값은 false 입니다. "); // flag 값은 false 입니다. 출력됨
         }
     }
 }
